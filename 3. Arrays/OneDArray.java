03b10ad7315c47058c90992b9ff95b5210bf1d18
dpkydv1999 <135200301+dpkydv1999@users.noreply.github.com>
2023-07-23 17:30:28 +0545

    Add files via upload

---
+++ 3. Arrays/OneDArray.java
@@ -0,0 +1,31 @@
+public class OneDArray {
+
+    public static void main(String[] args){
+
+        // int num[] = {1,2,3,4,5}; If we know the array values
+
+        int num1[] = new int[8];
+        num1[0] = 4;
+        num1[1] = 5;
+        // If we do not array values.
+
+        String names[] = {"Deepak", "yadav"};
+       
+
+        // System.out.println(num[0]);
+        // System.out.println(num[1]);
+        // System.out.println(num[2]);
+        // System.out.println(num[3]);
+    
+        for(int i=0; i<=1; i++){
+            // System.out.println(num[i]);
+
+            System.out.println(num1[i]);
+            System.out.println(names[i]);
+        }
+    
+    }
+
+    
+    
+}
