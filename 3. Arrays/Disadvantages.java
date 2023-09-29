1220fda8e969f9b6ee9acb753ec9ed054fe0affe
dpkydv1999 <ydv.dpk99@gmail.com>
2023-09-29 08:37:11 +0545

    Disadvantages of an array

---
+++ 3. Arrays/Disadvantages.java
@@ -0,0 +1,13 @@
+public class Disadvantages {
+    //1. size cannot be changed
+    //2. stores only homogeneous data elements
+    public static void main(String[] args) {
+        // not homogeneous data elements
+        // int nums[] = {1,2,3,5.6}; 
+        // System.out.println(nums[1]);
+        
+        int a[] = {2,3,4,5};
+        System.out.println(a[2]);
+    }
+    
+}
