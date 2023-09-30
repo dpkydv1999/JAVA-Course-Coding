33c28687f117a950ba8342c5cda60dd5fdd985a2
dpkydv1999 <ydv.dpk99@gmail.com>
2023-09-30 12:58:16 +0545

    Types of String

---
+++ 4. String/TypesOfString.java
@@ -0,0 +1,15 @@
+public class TypesOfString{
+    public static void main(String[] args) {
+        //1. Immutable String
+        String brand = "deepak";
+        System.out.println(brand);
+        brand.concat("Bengaluru");
+        System.out.println(brand); // No changes in String object
+
+        //2. Mutable String
+        StringBuilder brand1 = new StringBuilder("deepak");
+        System.out.println(brand1);
+        brand1.append("Kathmandu");
+        System.out.println(brand1);
+    }
+}
