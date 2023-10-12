20c4d1750f221d00450ca73076abe24669bed53b
dpkydv1999 <ydv.dpk99@gmail.com>
2023-10-12 17:49:18 +0545

    Hirarchial Inheritance

---
+++ 6. Inheritance, Polymorphism & Abstraction/Inheritance/Hierarchial.java
@@ -0,0 +1,23 @@
+//Hierarchial Inheritance
+package Inheritance;
+
+class Animal{
+    void display(){
+        System.out.println("Animal need good sleep");
+    }
+}
+class Tiger extends Animal{
+
+}
+class Monkey extends Animal{
+
+}
+class Donkey extends Animal{
+
+}
+public class Hierarchial {
+    public static void main(String[] args) {
+        Donkey d = new Donkey();
+        d.display();
+    }
+}
