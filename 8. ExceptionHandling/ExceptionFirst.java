7f7555c7e5a5afa1a02f4157847cb1864501aac5
dpkydv1999 <ydv.dpk99@gmail.com>
2023-11-01 09:04:41 +0545

    Exception

---
+++ 8. ExceptionHandling/ExceptionFirst.java
@@ -0,0 +1,8 @@
+//Syntactical Error
+public class ExceptionFirst
+{
+    public static void main(String[] args) 
+    {
+     System.out.printn();
+    }
+}
---
+++ 8. ExceptionHandling/TryCatch1.java
@@ -0,0 +1,21 @@
+//About Exception and how to handle it by using try and catch  
+public class TryCatch1 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 0;
+
+        int result =0;
+
+        try
+        {
+            result = num1/num2;
+            System.out.println("in try block");
+        }
+        catch(Exception e)
+        {
+            System.out.println("Something went wrong.." +e);
+        }
+        System.out.println(result);
+        System.out.println("bye");
+    }
+}
