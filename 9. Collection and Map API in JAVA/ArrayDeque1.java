d8eed4b03e83a7bd903c3224e374837da8b999fc
dpkydv1999 <ydv.dpk99@gmail.com>
2024-04-17 20:23:22 +0545

    array deque

--- 9. Collection and Map API in JAVA/ArrayDeque1.java
+++ 9. Collection and Map API in JAVA/ArrayDeque1.java
@@ -3,6 +3,25 @@ import java.util.*;
 public class ArrayDeque1 
 {
     public static void main(String[] args) {
-        
+        ArrayDeque ad1 = new ArrayDeque();
+
+        ad1.add(100);
+        ad1.add(200);
+        ad1.add(200);
+
+        System.out.println(ad1);
+
+        ad1.addFirst(10);
+        ad1.addLast(300);
+        ad1.add("java");
+
+        System.out.println(ad1);
+
+        ArrayDeque ad2 = new ArrayDeque();
+        ad2.offer(5);
+        ad2.offerFirst(3);
+        ad2.offerLast(10);
+
+        System.out.println(ad2);
     }
 }
